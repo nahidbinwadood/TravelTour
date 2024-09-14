50cfabe597ea8cca39530a9de15e8cbed67ef4d5
Akm Nahid Bin Wadood <nahidbinwadood@gmail.com>
2024-09-14 12:01:15 +0600

    added profile settings page

--- dist/assets/js/dashboard.js
+++ dist/assets/js/dashboard.js
@@ -1,4 +1,6 @@
 // Dashboard::Start
+
+//Theme change::Sta
 const toggleButton = document.getElementById("theme-toggle");
 const nightBtn = document.getElementById("night-mode");
 const lightBtn = document.getElementById("light-mode");
@@ -14,4 +16,44 @@ toggleButton?.addEventListener("click", () => {
     theme = "light";
   }
 });
+
+//Theme change::End
+
+// Image Preview::Start
+const imagePreview = document.querySelector(".image-preview");
+const imageFile = document.querySelector(".imageInput");
+const imageFileName = document.querySelector(".imageFileName");
+
+imageFile?.addEventListener("change", () => {
+  if (imageFile.files.length) {
+    const image = imageFile.files[0];
+    imageFileName.textContent = image?.name;
+    imagePreview.src = URL.createObjectURL(image);
+  }
+});
+// Image Preview::End
+
+// Pass Show/Hide::Start
+function togglePassVisibility(event) {
+  const button = event.currentTarget;
+  const inputId = button.getAttribute("data-input");
+  const inputField = document.getElementById(inputId);
+
+  if (button.classList.contains("showBtn")) {
+    inputField.type = "text";
+    button.classList.add("hidden");
+    button.nextElementSibling.classList.remove("hidden");
+  } else if (button.classList.contains("hideBtn")) {
+    inputField.type = "password";
+    button.classList.add("hidden");
+    button.previousElementSibling.classList.remove("hidden");
+  }
+}
+
+document.querySelectorAll(".showBtn, .hideBtn").forEach((button) => {
+  button?.addEventListener("click", togglePassVisibility);
+});
+
+// Pass Show/Hide::End
+
 // Dashboard::End
